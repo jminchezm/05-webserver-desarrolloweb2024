6e10649fea5788805f0e35905d2b7e3ba9c4bb86
JosueMinchez-M <josueminchezm@gmial.com>
2024-10-21 22:01:13 -0600

    Primer commit - Webserver listo

--- app15.js
+++ app15.js
@@ -5,3 +5,40 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+//Utilización de hbs para el renderizado de la página web
+app.set('view engine', 'hbs');
+
+//Uso de parciales "codigo que se repite en mas de un lugar o paginas web"
+hbs.registerPartials(__dirname + '/views/partials');
+
+//Servir contenido estático public3 donde está el templated
+app.use(express.static('public3')); //Middelware para la carpeta public3
+
+app.get('/', (req, res) => {
+    res.render('home3', {
+        nombre: 'Josué Mínchez',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Josué Mínchez',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Josué Mínchez',
+        titulo: 'Curso de Node'
+    });
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at htpp://localhost:${port}`);
+})
